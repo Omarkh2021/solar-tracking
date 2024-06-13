41b6ee5d5edbaeda7ad98103382ae1faac236cb1
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-06-13 19:20:33 +0300

    Add files via upload

---
+++ solar panel2.ino
@@ -0,0 +1,86 @@
+
+////////// solar tracking   //////////////
+#include <Servo.h> 
+Servo horizontal; // horizontal servo
+int servoh = 180; 
+int servohLimitHigh = 175;
+int servohLimitLow = 5;
+// 65 degrees MAX
+
+Servo vertical; // vertical servo
+int servov = 90; 
+int servovLimitHigh = 100;
+int servovLimitLow = 1;
+
+// LDR pin connections
+// name = analogpin;
+int ldrlt = A0; //LDR top left - BOTTOM LEFT <--- BDG
+int ldrrt = A3; //LDR top rigt - BOTTOM RIGHT
+int ldrld = A1; //LDR down left - TOP LEFT
+int ldrrd = A2; //LDR down rigt - TOP RIGHT
+
+void setup(){
+  ////////// solar tracking   //////////////
+horizontal.attach(5);//down servo
+vertical.attach(3);//top servo
+horizontal.write(180);
+vertical.write(90);
+delay(500);
+}
+void loop() {
+  ////////// solar tracking   //////////////
+int lt = analogRead(ldrlt); // top left
+int rt = analogRead(ldrrt); // top right
+int ld = analogRead(ldrld); // down left
+int rd = analogRead(ldrrd); // down right
+int dtime = 10; int tol = 90; // dtime=diffirence time, tol=toleransi
+int avt = (lt + rt) / 2; // average value top
+int avd = (ld + rd) / 2; // average value down
+int avl = (lt + ld) / 2; // average value left
+int avr = (rt + rd) / 2; // average value right
+int dvert = avt - avd; // check the diffirence of up and down
+int dhoriz = avl - avr;// check the diffirence og left and rigt
+
+if (-1*tol > dvert || dvert > tol) 
+ {
+ if (avt > avd)
+ {
+ servov = ++servov;
+ if (servov > servovLimitHigh)
+ {servov = servovLimitHigh;}
+ }
+ else if (avt < avd)
+ {servov= --servov;
+ if (servov < servovLimitLow)
+ { servov = servovLimitLow;}
+ }
+ vertical.write(servov);
+ }
+if (-1*tol > dhoriz || dhoriz > tol) // check if the diffirence is in the tolerance else change horizontal angle
+ {
+ if (avl > avr)
+ {
+ servoh = --servoh;
+ if (servoh < servohLimitLow)
+ {
+ servoh = servohLimitLow;
+ }
+ }
+ else if (avl < avr)
+ {
+ servoh = ++servoh;
+ if (servoh > servohLimitHigh)
+ {
+ servoh = servohLimitHigh;
+ }
+ }
+ else if (avl = avr)
+ {
+ delay(500);
+ }
+ horizontal.write(servoh);
+ }
+ 
+ delay(dtime);
+ 
+}
